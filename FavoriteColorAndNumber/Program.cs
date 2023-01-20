103e33685e3827e9eefd127e4bb5c68a31f26d38
amrit <amrit@AMRITSRAZERBLAD>
2023-01-20 11:06:00 -0500

    FavoriteColorsandNumbers

---
+++ FavoriteColorAndNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteColorAndNumber
+{
+    //Class: Program
+    //Author: Amrit Wanasundera
+    //Purpose: Console Read/Write and Exception-Handling Excercise
+    //Restrictions: None
+
+
+    /*
+     * This is a multiline comment
+     */
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Wanasundera_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wanasundera_HelloWorld
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("Amrit Wanasundera");
+            int x = 8;
+            int calculation1 = (20 * 6/x) / 100 + 56 * (23+x);
+            Console.WriteLine(calculation1);
+        }
+    }
+}
