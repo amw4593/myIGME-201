17c86c8fb6d662c014b2bfbc8ba762b61045c2d3
amrit <amrit@AMRITSRAZERBLAD>
2023-01-30 16:50:13 -0500

    #2

---
+++ PE05/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int operand1 = 15;
+            int operand2 = 12;
+            if (operand1 >= 10 && operand2 >= 10)
+            {
+                Console.WriteLine("Enter New Numbers!");
+            }
+                Console.WriteLine("Operand1: ");
+            if (operand1 >= 10){
+                Console.WriteLine(true);
+            }else
+            {
+                Console.WriteLine(false);
+            }
+            Console.WriteLine("Operand2: ");
+            if (operand2 >= 10){
+                Console.WriteLine(true);
+            }else
+            {
+                Console.WriteLine(false);
+            };
+
+        }
+    }
+}
