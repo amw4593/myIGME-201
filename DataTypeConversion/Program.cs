20707219c7d078e74400bc3c282b07007822bcca
amrit <amrit@AMRITSRAZERBLAD>
2023-02-02 20:30:28 -0500

    PE06

--- DataTypeConversion/Program.cs
+++ DataTypeConversion/Program.cs
@@ -11,8 +11,62 @@ namespace DataTypeConversion
 
         static void Main(string[] args)
         {
-            Console.Out.WriteLine("50 plus 25 is " + 50 + 25);
+            // declare the unsorted and sorted arrays
+            double[] aUnsorted = new double[5];
+            double[] aSorted = new double[5];
 
+            //Data Validation
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter a list of space-separated numbers");
+
+                    // read the space-separated string of numbers
+                    string sNumberString = Console.ReadLine();
+
+                    // split the string into the an array of strings which are the individual numbers
+                    string[] sNumbers = sNumberString.Split(' ');
+
+                    //try to parse each value
+                    for (int i = 0; i < sNumberString.Length; i++)
+                    {
+                        aUnsorted[i] = double.Parse(sNumbers[i]);
+                    }
+
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter valid numbers");
+                }
+            }
+            // SORTING
+            // loop through unsorted array
+            for (int i = 0; i < aSorted.Length; i++)
+            {
+                double tempSortInt = aUnsorted[0];
+                for (int j = 1; j < aUnsorted.Length; j++)
+                {
+                    // Checks already sorted numbers
+                    for (int k = 0; k < aSorted.Length; k++)
+                    {
+                        // If it exists in the sorted array, skip to the next number
+                        if (aSorted[k] == tempSortInt)
+                        {
+                            tempSortInt = aUnsorted[j];
+                            // I hope this only breaks out of the current loop.
+                            break;
+                        }
+                    }
+                    //sorts the unsorted loops 
+                    if (tempSortInt < aUnsorted[j])
+                    {
+                        tempSortInt = aUnsorted[j];
+                    }
+                }
+                aSorted[i] = tempSortInt;
+            }
         }
     }
 }
---
+++ PE06/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            // generate a random number between 0 inclusive and 101 exclusive
+            int randomNumber = rand.Next(0, 101);
+            Console.WriteLine(randomNumber.ToString());
+
+            for(int i = 0; i < 8; i++) 
+            {
+                Console.Write("Pick a Number Between 1 and 100: ");
+                Console.ReadLine(randomNumber);
+            }
+        }
+    }
+}
