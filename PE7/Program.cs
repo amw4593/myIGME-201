97c9f3a7bb3d06180c6ba0434e5f5d0426f033c5
amrit <amrit@AMRITSRAZERBLAD>
2023-02-08 18:19:47 -0500

    PE8

--- PE7/Program.cs
+++ PE7/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PE7
 {
@@ -10,6 +11,12 @@ namespace PE7
     {
         static void Main(string[] args)
         {
+            string[] blab = new string[5];
+            string[5] = 5th string;
+
+
+
+
         }
     }
 }
---
+++ PE8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,,] computations = new double[3, 3, 3];
+
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    for (int z = 0; z < 3; z++)
+                    {
+                        computations[x, y, z] = x + y + z;
+                    }
+                }
+            }
+        }
+    }
+}
