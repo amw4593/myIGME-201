7c6966518321da07749298b75d2898c5742044ee
amrit <amrit@AMRITSRAZERBLAD>
2023-02-27 21:36:08 -0500

    p

---
+++ GameofLife/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameofLife
+{
+    public enum EAliveState
+    {
+        alive, 
+        dead
+    }
+
+    public enum EInfectedState
+    {
+        organic, 
+        vaccinated, 
+        infected
+    }
+
+    public enum EDirection
+    {
+        right,
+        down, 
+        left, 
+        up, 
+        topLeft,
+        topRight,
+        bottomLeft,
+        bottomRight
+    }
+
+    public struct StructCellState
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
