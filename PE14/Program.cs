9f2e8eb03c2a679797dbad15906842255db9388c
amrit <amrit@AMRITSRAZERBLAD>
2023-03-07 21:41:12 -0500

    PE16

--- PE14/Program.cs
+++ PE14/Program.cs
@@ -10,6 +10,84 @@ namespace PE14
     {
         static void Main(string[] args)
         {
+            //1
+            public abstract class MyAbstractClass
+        {
+            private int xY;
+
+            public int MyProperty
+            {
+                get { return xY; }
+                set { xY = value; }
+            }
+        }
+        // 2
+        public interface IMyInterface
+        {
+            void MyMethod();
+        }
+
+        public class MyClass : IMyInterface
+        {
+            public void MyMethod()
+            {
+
+            }
+        }
+
+        public class MyClass2 : IMyInterface
+        {
+            public void MyMethod()
+            {
+
+            }
+        }
+//3
+namespace myNamespace
+    {
+        public interface IMyInterface
+        {
+            void MyMethod();
+        }
+
+        public class MyClass : IMyInterface
+        {
+            public void MyMethod()
+            {
+                
+            }
+        }
+
+        public class MyClass2 : IMyInterface
+        {
+            public void MyMethod()
+            {
+      
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+           
+                MyClass myClass = new MyClass();
+                CallMyMethod(myClass);
+
+                MyClass2 myClass2 = new MyClass2();
+                CallMyMethod(myClass2);
+
+                Console.ReadLine();
+            }
+
+            public static void CallMyMethod(IMyInterface myObject)
+            {
+                myObject.MyMethod();
+            }
         }
     }
+
+
+}
+}
 }
